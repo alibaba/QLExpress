f23205a605d86bf45a0e5f4c2935be3e5343339f
杨啸林 <1287124582@qq.com>
2021-01-03 14:12:14 +0800

    修复break、continue部分逻辑下死循环或下标超出栈容量的问题

--- src/main/java/com/ql/util/express/instruction/ForRelBreakContinue.java
+++ src/main/java/com/ql/util/express/instruction/ForRelBreakContinue.java
@@ -9,4 +9,13 @@ public class ForRelBreakContinue{
 	 List<InstructionGoTo> breakList = new ArrayList<InstructionGoTo>();
 	 List<InstructionGoTo> continueList = new ArrayList<InstructionGoTo>();
 
+	public void sign(){
+		for(InstructionGoTo instructionGoTo : this.breakList){
+			instructionGoTo.setMoveTimes(instructionGoTo.getMoveTimes()+1);
+		}
+		for(InstructionGoTo instructionGoTo : this.continueList){
+			instructionGoTo.setMoveTimes(instructionGoTo.getMoveTimes()+1);
+		}
+	}
+
 }
