61e2dcbe5712234ac67d2214b8203448f8a5c828
supremewang <wong-son@163.com>
2021-01-04 20:38:11 +0800

    instructionFactory map没有put

--- src/main/java/com/ql/util/express/instruction/InstructionFactory.java
+++ src/main/java/com/ql/util/express/instruction/InstructionFactory.java
@@ -17,6 +17,7 @@ public abstract class InstructionFactory {
 			if (result == null) {
 				result = (InstructionFactory) Class.forName(factory)
 						.newInstance();
+
 				instructionFactory.put(factory, result);
 			}
 			return result;
