fd7e41eb45ef066b03510cdea93c7a51e8ebf51f
DQinYuan <932087612@qq.com>
2021-08-22 22:37:46 +0800

    support java8 stream api

---
+++ src/main/java/com/ql/util/express/QLambdaInvocationHandler.java
@@ -0,0 +1,20 @@
+package com.ql.util.express;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+
+public class QLambdaInvocationHandler implements InvocationHandler {
+    private final QLambda qLambda;
+
+    public QLambdaInvocationHandler(QLambda qLambda) {
+        this.qLambda = qLambda;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        return Modifier.isAbstract(method.getModifiers())? qLambda.call(args):
+                // 为了应对 toString 方法
+                method.getReturnType() == String.class? "QLambdaProxy": null;
+    }
+}
---
+++ src/test/java/com/ql/util/express/test/LambdaTest.java
@@ -0,0 +1,64 @@
+package com.ql.util.express.test;
+
+import com.ql.util.express.DefaultContext;
+import com.ql.util.express.ExpressRunner;
+import com.ql.util.express.Operator;
+import com.ql.util.express.QLambda;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class LambdaTest {
+
+    @Test
+    public void lambdaAddTest() throws Exception {
+        String express = "la = (a,b)->a+b; la.apply(1,2)";
+        ExpressRunner runner = new ExpressRunner(false, true);
+        DefaultContext<String, Object> context = new DefaultContext<String, Object>();
+
+        Object res = runner.execute(express, context, null, true, true);
+        Assert.assertEquals(3, res);
+    }
+
+    @Test
+    public void lambdaOperatorTest() throws Exception {
+        ExpressRunner runner = new ExpressRunner(false, true);
+        runner.addOperator("map", new Operator() {
+            @Override
+            public Object executeInner(Object[] list) throws Exception {
+                List<?> origin = (List<?>) list[0];
+                QLambda qLambda = (QLambda) list[1];
+                List result = new ArrayList();
+                for (int i = 0; i < origin.size(); i++) {
+                    result.add(qLambda.call(origin.get(i), i));
+                }
+                return result;
+            }
+        });
+
+        DefaultContext<String, Object> context = new DefaultContext<String, Object>();
+        context.put("l", Arrays.asList("a", "b", "c"));
+        Object res = runner.execute("l map (item, index)->item+index", context, null,
+                true, true);
+        Assert.assertEquals(Arrays.asList("a0", "b1", "c2"), res);
+
+        Object res1 = runner.execute("l map (item,index)->item+index", context,
+                null, true, true);
+        Assert.assertEquals(Arrays.asList("a0", "b1", "c2"), res1);
+    }
+
+    @Test
+    public void streamTest() throws Exception {
+        String expr = "a = NewList(1,2,3);a.stream().filter(item -> item > 1)" +
+                ".map(item->item+1)" +
+                ".collect(Collectors.toList())";
+        ExpressRunner runner = new ExpressRunner(false, true);
+        Object res = runner.execute(expr, new DefaultContext<String, Object>(), null,
+                false, true);
+        Assert.assertEquals(Arrays.asList(3, 4), res);
+    }
+
+}
