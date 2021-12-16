d9ca99a79c4714637098416cb63cc52766936ebc
yumin.pym <yumin.pym@alibaba-inc.com>
2021-12-16 21:22:17 +0800

    code format

--- src/test/java/com/ql/util/express/test/AClassDefineSingleTest.java
+++ src/test/java/com/ql/util/express/test/AClassDefineSingleTest.java
@@ -7,20 +7,20 @@ import org.junit.Test;
 public class AClassDefineSingleTest {
     @Test
     public void testABC() throws Exception {
-        String expressDefine =
-            "class ABC(com.ql.util.express.test.BeanExample bean,String name){"
-                + "整数值:bean.intValue;"
-                + "};";
-        String express =
-            "ABC example = new ABC(new com.ql.util.express.test.BeanExample(),'xuannan');"
-                + " example.整数值 =100 + 100;"
-                + " print(example.整数值);"
-                + "";
+        String expressDefine = ""
+            + "class ABC(com.ql.util.express.test.BeanExample bean, String name) {"
+            + "    整数值:bean.intValue;"
+            + "};";
+
+        String express = ""
+            + "ABC example = new ABC(new com.ql.util.express.test.BeanExample(), 'xuannan');"
+            + " example.整数值 =100 + 100;"
+            + " print(example.整数值);";
+
         ExpressRunner runner = new ExpressRunner(false, true);
         DefaultContext<String, Object> context = new DefaultContext<>();
         runner.loadMultiExpress("", expressDefine);
         runner.loadMultiExpress("ClassTest", express);
-        Object r = runner.executeByExpressName("ClassTest", context,
-            null, true, false, null);
+        Object r = runner.executeByExpressName("ClassTest", context, null, true, false, null);
     }
 }
