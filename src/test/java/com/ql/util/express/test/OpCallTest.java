ea6ada0444f34fc1fb1ee742f1eae3a6b110fcb7
yumin.pym <yumin.pym@alibaba-inc.com>
2022-01-06 17:23:59 +0800

    format test code

--- src/test/java/com/ql/util/express/test/OpCallTest.java
+++ src/test/java/com/ql/util/express/test/OpCallTest.java
@@ -14,8 +14,7 @@ public class OpCallTest {
         runner.addOperator("@love", new LoveOperator("@love"));
         runner.loadMultiExpress(null, "function abc(String s){println(s)}");
         runner.addOperatorWithAlias("打印", "println", null);
-        runner.addFunctionOfClassMethod("isVIP", BeanExample.class.getName(),
-            "isVIP", new Class[] {String.class}, "");
+        runner.addFunctionOfClassMethod("isVIP", BeanExample.class.getName(), "isVIP", new Class[] {String.class}, "");
         runner.addOperatorWithAlias("是否VIP", "isVIP", "亲爱的$1,你还不是VIP用户");
 
         String[][] expressTest = new String[][] {
@@ -26,26 +25,19 @@ public class OpCallTest {
             {"isVIP (\"玄难\")", "false"},
             {"是否VIP (\"玄难\")", "false"},
         };
-        IExpressContext<String, Object> expressContext = new ExpressContextExample(
-            null);
+        IExpressContext<String, Object> expressContext = new ExpressContextExample(null);
 
         for (int point = 0; point < expressTest.length; point++) {
             String expressStr = expressTest[point][0];
             List<String> errorList = new ArrayList<>();
-            Object result = runner.execute(expressStr, expressContext, errorList,
-                false, true);
-            if (result == null
-                && !expressTest[point][1].equalsIgnoreCase("null")
-                || expressTest[point][1].equalsIgnoreCase("null")
-                && result != null
-                || result != null
-                && !expressTest[point][1]
-                .equalsIgnoreCase(result.toString())) {
-                throw new Exception("处理错误,计算结果与预期的不匹配:" + expressStr + " = "
-                    + result + "但是期望值是：" + expressTest[point][1]);
+            Object result = runner.execute(expressStr, expressContext, errorList, false, true);
+            if (result == null && !expressTest[point][1].equalsIgnoreCase("null")
+                || expressTest[point][1].equalsIgnoreCase("null") && result != null
+                || result != null && !expressTest[point][1].equalsIgnoreCase(result.toString())) {
+                throw new Exception(
+                    "处理错误,计算结果与预期的不匹配:" + expressStr + " = " + result + "但是期望值是：" + expressTest[point][1]);
             }
-            System.out.println("Example " + point + " : " + expressStr + " =  "
-                + result);
+            System.out.println("Example " + point + " : " + expressStr + " =  " + result);
             if (errorList.size() > 0) {
                 System.out.println("\t\t系统输出的错误提示信息:" + errorList);
             }
