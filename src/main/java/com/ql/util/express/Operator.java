9a6d1fc708c9e3c3f1bba25026140b4417f1f8f4
v1stl <v1.lion@qq.com>
2022-01-16 11:57:58 +0800

    fix: character equal to string problem. (#174)

--- src/main/java/com/ql/util/express/Operator.java
+++ src/main/java/com/ql/util/express/Operator.java
@@ -1,11 +1,11 @@
 package com.ql.util.express;
 
-import java.util.Date;
-
 import com.ql.util.express.config.QLExpressRunStrategy;
 import com.ql.util.express.exception.QLException;
 import com.ql.util.express.instruction.OperateDataCacheManager;
 import com.ql.util.express.instruction.op.OperatorBase;
+
+import java.util.Date;
 
 /**
  * 操作符的基类
@@ -13,6 +13,90 @@ import com.ql.util.express.instruction.op.OperatorBase;
  * @author xuannan
  */
 public abstract class Operator extends OperatorBase {
+    /**
+     * 进行对象是否相等的比较
+     *
+     * @param op1
+     * @param op2
+     * @return
+     */
+    public static boolean objectEquals(Object op1, Object op2) {
+        if (op1 == null && op2 == null) {
+            return true;
+        }
+        if (op1 == null || op2 == null) {
+            return false;
+        }
+
+        //Character的值比较
+        if (op1 instanceof Character || op2 instanceof Character) {
+            if (op1 instanceof Character && op2 instanceof Character) {
+                return op1.equals(op2);
+            } else if (op1 instanceof Number) {
+                return OperatorOfNumber.compareNumber((Number) op1, (int) (Character) op2) == 0;
+            } else if (op2 instanceof Number) {
+                return OperatorOfNumber.compareNumber((int) (Character) op1, (Number) op2) == 0;
+            } else if (op1 instanceof String) {
+                return op2.toString().equals(op1);
+            } else if (op2 instanceof String) {
+                return op1.toString().equals(op2);
+            }
+        }
+        //数值的值比较
+        if (op1 instanceof Number && op2 instanceof Number) {
+            //数字比较
+            return OperatorOfNumber.compareNumber((Number) op1, (Number) op2) == 0;
+        }
+        //调用原始Object的比较
+        return op1.equals(op2);
+    }
+
+    /**
+     * 进行对象比较
+     *
+     * @param op1
+     * @param op2
+     * @return 0 等于 ，负数 小于 , 正数 大于
+     * @throws Exception
+     */
+    public static int compareData(Object op1, Object op2) throws Exception {
+        if (op1 == op2) {
+            return 0;
+        }
+
+        int compareResult;
+
+        if (op1 instanceof String) {
+            compareResult = ((String) op1).compareTo(op2.toString());
+        } else if (op2 instanceof String) {
+            compareResult = op1.toString().compareTo((String) op2);
+        } else if (op1 instanceof Character || op2 instanceof Character) {
+            if (op1 instanceof Character && op2 instanceof Character) {
+                compareResult = ((Character) op1).compareTo((Character) op2);
+            } else if (op1 instanceof Number) {
+                compareResult = OperatorOfNumber.compareNumber((Number) op1, (int) (Character) op2);
+            } else if (op2 instanceof Number) {
+                compareResult = OperatorOfNumber.compareNumber((int) (Character) op1, (Number) op2);
+            } else {
+                throw new QLException(op1 + "和" + op2 + "不能执行compare 操作");
+            }
+        } else if (op1 instanceof Number && op2 instanceof Number) {
+            //数字比较
+            compareResult = OperatorOfNumber.compareNumber((Number) op1, (Number) op2);
+        } else if ((op1 instanceof Boolean) && (op2 instanceof Boolean)) {
+            if (((Boolean) op1).booleanValue() == ((Boolean) op2).booleanValue()) {
+                compareResult = 0;
+            } else {
+                compareResult = -1;
+            }
+        } else if ((op1 instanceof Date) && (op2 instanceof Date)) {
+            compareResult = ((Date) op1).compareTo((Date) op2);
+        } else {
+            throw new QLException(op1 + "和" + op2 + "不能执行compare 操作");
+        }
+        return compareResult;
+    }
+
     @Override
     public OperateData executeInner(InstructionSetContext parent, ArraySwap list) throws Exception {
         Object[] parameters = new Object[list.length];
@@ -37,88 +121,4 @@ public abstract class Operator extends OperatorBase {
     }
 
     public abstract Object executeInner(Object[] list) throws Exception;
-
-    /**
-     * 进行对象是否相等的比较
-     *
-     * @param op1
-     * @param op2
-     * @return
-     */
-    public static boolean objectEquals(Object op1, Object op2) {
-        if (op1 == null && op2 == null) {
-            return true;
-        }
-        if (op1 == null || op2 == null) {
-            return false;
-        }
-
-        //Character的值比较
-        if (op1 instanceof Character || op2 instanceof Character) {
-            int compareResult;
-            if (op1 instanceof Character && op2 instanceof Character) {
-                return op1.equals(op2);
-            } else if (op1 instanceof Number) {
-                compareResult = OperatorOfNumber.compareNumber((Number)op1, (int)(Character)op2);
-                return compareResult == 0;
-            } else if (op2 instanceof Number) {
-                compareResult = OperatorOfNumber.compareNumber((int)(Character)op1, (Number)op2);
-                return compareResult == 0;
-            }
-        }
-        //数值的值比较
-        if (op1 instanceof Number && op2 instanceof Number) {
-            //数字比较
-            int compareResult = OperatorOfNumber.compareNumber((Number)op1, (Number)op2);
-            return compareResult == 0;
-        }
-        //调用原始Object的比较
-        return op1.equals(op2);
-    }
-
-    /**
-     * 进行对象比较
-     *
-     * @param op1
-     * @param op2
-     * @return 0 等于 ，负数 小于 , 正数 大于
-     * @throws Exception
-     */
-    public static int compareData(Object op1, Object op2) throws Exception {
-        if (op1 == op2) {
-            return 0;
-        }
-
-        int compareResult;
-
-        if (op1 instanceof String) {
-            compareResult = ((String)op1).compareTo(op2.toString());
-        } else if (op2 instanceof String) {
-            compareResult = op1.toString().compareTo((String)op2);
-        } else if (op1 instanceof Character || op2 instanceof Character) {
-            if (op1 instanceof Character && op2 instanceof Character) {
-                compareResult = ((Character)op1).compareTo((Character)op2);
-            } else if (op1 instanceof Number) {
-                compareResult = OperatorOfNumber.compareNumber((Number)op1, (int)(Character)op2);
-            } else if (op2 instanceof Number) {
-                compareResult = OperatorOfNumber.compareNumber((int)(Character)op1, (Number)op2);
-            } else {
-                throw new QLException(op1 + "和" + op2 + "不能执行compare 操作");
-            }
-        } else if (op1 instanceof Number && op2 instanceof Number) {
-            //数字比较
-            compareResult = OperatorOfNumber.compareNumber((Number)op1, (Number)op2);
-        } else if ((op1 instanceof Boolean) && (op2 instanceof Boolean)) {
-            if (((Boolean)op1).booleanValue() == ((Boolean)op2).booleanValue()) {
-                compareResult = 0;
-            } else {
-                compareResult = -1;
-            }
-        } else if ((op1 instanceof Date) && (op2 instanceof Date)) {
-            compareResult = ((Date)op1).compareTo((Date)op2);
-        } else {
-            throw new QLException(op1 + "和" + op2 + "不能执行compare 操作");
-        }
-        return compareResult;
-    }
 }
--- src/test/java/com/ql/util/express/example/OperatorTest.java
+++ src/test/java/com/ql/util/express/example/OperatorTest.java
@@ -12,6 +12,51 @@ import org.junit.Test;
  * 本例用于展示如何自定义操作符和方法
  */
 public class OperatorTest {
+
+    @Test
+    public void testCharacterStringEqualsTrue() throws Exception {
+        DefaultContext<String, Object> context = new DefaultContext<>();
+        String express = "'1' == Borrower_creditLevel";
+        context.put("Borrower_creditLevel", "1");
+
+        ExpressRunner runner = new ExpressRunner();
+        boolean r = (Boolean) runner.execute(express, context, null, false, false);
+        Assert.assertTrue("字符串与字符不相等", r);
+    }
+
+    @Test
+    public void testStringCharacterEqualsTrue() throws Exception {
+        DefaultContext<String, Object> context = new DefaultContext<>();
+        String express = "Borrower_creditLevel == '1'";
+        context.put("Borrower_creditLevel", "1");
+
+        ExpressRunner runner = new ExpressRunner();
+        boolean r = (Boolean) runner.execute(express, context, null, false, false);
+        Assert.assertTrue("字符串与字符不相等", r);
+    }
+
+    @Test
+    public void testStringCharacterEqualsNotTrue() throws Exception {
+        DefaultContext<String, Object> context = new DefaultContext<>();
+        String express = "Borrower_creditLevel == ''";
+        context.put("Borrower_creditLevel", "1");
+
+        ExpressRunner runner = new ExpressRunner();
+        boolean r = (Boolean) runner.execute(express, context, null, false, false);
+        Assert.assertFalse("字符串与字符相等", r);
+    }
+
+    @Test
+    public void testCharacterStringEqualsNotTrue() throws Exception {
+        DefaultContext<String, Object> context = new DefaultContext<>();
+        String express = "'' == Borrower_creditLevel";
+        context.put("Borrower_creditLevel", "1");
+
+        ExpressRunner runner = new ExpressRunner();
+        boolean r = (Boolean) runner.execute(express, context, null, false, false);
+        Assert.assertFalse("字符串与字符相等", r);
+    }
+
     /**
      * 定义一个简单的二元操作符
      *
@@ -24,7 +69,7 @@ public class OperatorTest {
         ExpressRunner runner = new ExpressRunner();
         //定义操作符addT，其实现为AddTwiceOperator
         runner.addOperator("addT", new AddTwiceOperator());
-        int r = (Integer)runner.execute(exp, null, null, false, false);
+        int r = (Integer) runner.execute(exp, null, null, false, false);
         System.out.println(r);
         Assert.assertEquals("操作符执行错误", 49, r);
     }
@@ -41,7 +86,7 @@ public class OperatorTest {
         ExpressRunner runner = new ExpressRunner();
         //定义操作符addN，其实现为AddNOperator，语法格式与in一致
         runner.addOperator("addN", "in", new AddNOperator());
-        int r = (Integer)runner.execute(exp, null, null, false, false);
+        int r = (Integer) runner.execute(exp, null, null, false, false);
         System.out.println(r);
         Assert.assertEquals("操作符执行错误", 10, r);
     }
@@ -58,7 +103,7 @@ public class OperatorTest {
         ExpressRunner runner = new ExpressRunner();
         //定义方法addN，其实现为AddNOperator
         runner.addFunction("addN", new AddNOperator());
-        int r = (Integer)runner.execute(exp, null, null, false, false);
+        int r = (Integer) runner.execute(exp, null, null, false, false);
         System.out.println(r);
         Assert.assertEquals("操作符执行错误", 10, r);
     }
@@ -79,7 +124,7 @@ public class OperatorTest {
         ExpressRunner runner = new ExpressRunner();
         //定义操作符addT，其实现为AddTwiceOperator
         runner.addOperator("addT", new AddTwiceOperator());
-        int r = (Integer)runner.execute(exp, expressContext, null, false, false);
+        int r = (Integer) runner.execute(exp, expressContext, null, false, false);
         System.out.println(r);
         Assert.assertEquals("操作符执行错误", 49, r);
     }
