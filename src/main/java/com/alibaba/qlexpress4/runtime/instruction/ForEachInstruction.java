d231151a803eee175876c074b5dc1725f54846cd
Dqy <932087612@qq.com>
2022-05-15 21:27:06 +0800

    for each instruction

--- src/main/java/com/alibaba/qlexpress4/runtime/instruction/ForEachInstruction.java
+++ src/main/java/com/alibaba/qlexpress4/runtime/instruction/ForEachInstruction.java
@@ -2,18 +2,23 @@ package com.alibaba.qlexpress4.runtime.instruction;
 
 import com.alibaba.qlexpress4.QLOptions;
 import com.alibaba.qlexpress4.exception.ErrorReporter;
+import com.alibaba.qlexpress4.runtime.QLambda;
 import com.alibaba.qlexpress4.runtime.QRuntime;
 
 /**
- * @Operation: get next element from Iterator at top of stack
+ * @Operation: process each element in iterable object on top of stack,
  * @Input: 1
- * @Output: 1
+ * @Output: 0
  *
  * Author: DQinYuan
  */
-public class NextInstruction extends QLInstruction {
-    public NextInstruction(ErrorReporter errorReporter) {
+public class ForEachInstruction extends QLInstruction {
+
+    private final QLambda body;
+
+    public ForEachInstruction(ErrorReporter errorReporter, QLambda body) {
         super(errorReporter);
+        this.body = body;
     }
 
     @Override
