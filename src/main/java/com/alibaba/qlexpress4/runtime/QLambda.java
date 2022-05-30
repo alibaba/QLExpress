80bb4b1ee47a72cffa56e660bf65bd84813d1a9b
Dqy <932087612@qq.com>
2022-05-30 15:53:10 +0800

    qlambda call throw exception

--- src/main/java/com/alibaba/qlexpress4/runtime/QLambda.java
+++ src/main/java/com/alibaba/qlexpress4/runtime/QLambda.java
@@ -5,20 +5,35 @@ import java.util.function.Predicate;
 
 public interface QLambda extends Runnable, Predicate<Object>, Function<Object, Object> {
 
-    QResult call(Object... params);
+    QResult call(Object... params) throws Exception;
 
     @Override
     default void run() {
-        call();
+        try {
+            call();
+        } catch (Exception e) {
+            throw e instanceof RuntimeException?
+                    (RuntimeException) e: new RuntimeException(e);
+        }
     }
 
     @Override
     default boolean test(Object o) {
-        return (boolean) call(o).getResult().get();
+        try {
+            return (boolean) call(o).getResult().get();
+        } catch (Exception e) {
+            throw e instanceof RuntimeException?
+                    (RuntimeException) e: new RuntimeException(e);
+        }
     }
 
     @Override
     default Object apply(Object o) {
-        return call(o).getResult().get();
+        try {
+            return call(o).getResult().get();
+        } catch (Exception e) {
+            throw e instanceof RuntimeException?
+                    (RuntimeException) e: new RuntimeException(e);
+        }
     }
 }
