87466c08d65861d57b20e58c70d62fa860fbceda
Dqy <932087612@qq.com>
2022-06-04 13:15:47 +0800

    add DataValue

---
+++ src/main/java/com/alibaba/qlexpress4/runtime/data/DataValue.java
@@ -0,0 +1,21 @@
+package com.alibaba.qlexpress4.runtime.data;
+
+import com.alibaba.qlexpress4.runtime.Value;
+
+/**
+ * @Author TaoKan
+ * @Date 2022/5/6 下午6:29
+ */
+public class DataValue implements Value {
+
+    private final Object value;
+
+    public DataValue(Object value) {
+        this.value = value;
+    }
+
+    @Override
+    public Object get() {
+        return this.value;
+    }
+}
