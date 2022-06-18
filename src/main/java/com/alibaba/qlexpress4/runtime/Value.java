06d2b078a1647d8a185b8d532ba06fe4f49e465a
Dqy <932087612@qq.com>
2022-06-19 00:48:22 +0800

    fix test npe

--- src/main/java/com/alibaba/qlexpress4/runtime/Value.java
+++ src/main/java/com/alibaba/qlexpress4/runtime/Value.java
@@ -6,7 +6,7 @@ import com.alibaba.qlexpress4.runtime.data.DataValue;
  * Author: DQinYuan
  */
 public interface Value {
-    Value NULL_VALUE = new DataValue(null);
+    Value NULL_VALUE = new DataValue((Object) null);
 
     Object get();
 
