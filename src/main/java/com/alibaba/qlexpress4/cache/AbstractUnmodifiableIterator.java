1581704b8385c6907b550188e0084e25241e4062
taokankan.tk <taokan1991@aliyun.com>
2022-06-22 19:56:16 +0800

    new lru Iter component

---
+++ src/main/java/com/alibaba/qlexpress4/cache/AbstractUnmodifiableIterator.java
@@ -0,0 +1,32 @@
+package com.alibaba.qlexpress4.cache;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * AbstractUnmodifiableIterator
+ * @Author TaoKan
+ * @Date 2022/6/11 下午8:05
+ */
+public abstract class AbstractUnmodifiableIterator<E> implements Iterator<E> {
+    private E next;
+
+    protected AbstractUnmodifiableIterator(E firstEntity) {
+        this.next = firstEntity;
+    }
+    public final boolean hasNext() {
+        return next != null;
+    }
+
+    @Override
+    public final E next() {
+        if (next == null) {
+            throw new NoSuchElementException();
+        }
+        E old = next;
+        next = computeNext(old);
+        return old;
+    }
+
+    protected abstract E computeNext(E previous);
+}
