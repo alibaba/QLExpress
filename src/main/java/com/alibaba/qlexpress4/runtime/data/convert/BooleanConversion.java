e6f6f7499e9bf782b981eeeedf71d28e9e54c08a
yumin.pym <yumin.pym@alibaba-inc.com>
2022-08-17 16:41:26 +0800

    commit

--- src/main/java/com/alibaba/qlexpress4/runtime/data/convert/BooleanConversion.java
+++ src/main/java/com/alibaba/qlexpress4/runtime/data/convert/BooleanConversion.java
@@ -1,6 +1,5 @@
 package com.alibaba.qlexpress4.runtime.data.convert;
 
-
 import com.alibaba.qlexpress4.runtime.data.implicit.QLConvertResult;
 import com.alibaba.qlexpress4.runtime.data.implicit.QLConvertResultType;
 
@@ -11,10 +10,10 @@ import com.alibaba.qlexpress4.runtime.data.implicit.QLConvertResultType;
 public class BooleanConversion {
     public static QLConvertResult trans(Object object) {
         //delete null to boolean
+        // TODO idea提示
         if (object.getClass() == boolean.class || object.getClass() == Boolean.class) {
-            return new QLConvertResult(QLConvertResultType.CAN_TRANS,(Boolean) object);
+            return new QLConvertResult(QLConvertResultType.CAN_TRANS, object);
         }
         return new QLConvertResult(QLConvertResultType.NOT_TRANS, null);
     }
-
 }
