0d36e191d32a14056408db81d719e795e382ca94
yumin.pym <yumin.pym@alibaba-inc.com>
2022-11-02 19:43:56 +0800

    commit

--- src/main/java/com/alibaba/qlexpress4/runtime/LeftValue.java
+++ src/main/java/com/alibaba/qlexpress4/runtime/LeftValue.java
@@ -11,12 +11,11 @@ import com.alibaba.qlexpress4.runtime.data.implicit.QLConvertResultType;
  * Author: DQinYuan
  */
 public interface LeftValue extends Value {
-
-    default QLConvertResult set(Object newValue, ErrorReporter errorReporter){
+    default QLConvertResult set(Object newValue, ErrorReporter errorReporter) {
         Class<?> defineType = getDefinedType();
         QLConvertResult result = InstanceConversion.castObject(newValue, defineType);
-        if(result.getResultType().equals(QLConvertResultType.NOT_TRANS)){
-            throw errorReporter.report("TRANS_OBJECT_ERROR","can not trans to:"+defineType.getName());
+        if (result.getResultType().equals(QLConvertResultType.NOT_TRANS)) {
+            throw errorReporter.report("TRANS_OBJECT_ERROR", "can not trans to:" + defineType.getName());
         }
         setInner(result.getCastValue());
         return result;
