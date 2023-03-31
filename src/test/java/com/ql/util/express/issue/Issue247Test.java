522038a2c291b10f0bfde96b4fdaaae592728e9d
yumin.pym <yumin.pym@alibaba-inc.com>
2023-03-31 18:13:31 +0800

    modify com.ql.util.express.issue.Issue247Test

--- src/test/java/com/ql/util/express/issue/Issue247Test.java
+++ src/test/java/com/ql/util/express/issue/Issue247Test.java
@@ -1,39 +1,60 @@
 package com.ql.util.express.issue;
 
+import java.util.Arrays;
+import java.util.List;
+
 import com.ql.util.express.DefaultContext;
 import com.ql.util.express.ExpressRunner;
 import com.ql.util.express.IExpressContext;
 import com.ql.util.express.config.QLExpressRunStrategy;
-import org.junit.AfterClass;
+import com.ql.util.express.exception.QLException;
 import org.junit.Assert;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
  * @author 冰够
  */
 public class Issue247Test {
-    private static boolean compareNullLessMoreAsFalse;
-
-    @BeforeClass
-    public static void beforeClass() {
-        compareNullLessMoreAsFalse = QLExpressRunStrategy.isCompareNullLessMoreAsFalse();
-        QLExpressRunStrategy.setCompareNullLessMoreAsFalse(true);
-    }
-
-    @AfterClass
-    public static void afterClass() {
-        QLExpressRunStrategy.setCompareNullLessMoreAsFalse(compareNullLessMoreAsFalse);
-    }
-
     @Test
     public void test() throws Exception {
-        ExpressRunner runner = new ExpressRunner(false, true);
-        String expression = "2 > null";
+        boolean compareNullLessMoreAsFalse = QLExpressRunStrategy.isCompareNullLessMoreAsFalse();
+        try {
+            IExpressContext<String, Object> context = new DefaultContext<>();
+            ExpressRunner runner = new ExpressRunner(false, true);
 
-        IExpressContext<String, Object> context = new DefaultContext<>();
-        Object result = runner.execute(expression, context, null, false, false);
-        Assert.assertFalse((Boolean)result);
+            boolean result = (boolean)runner.execute("null == null", context, null, false, false);
+            Assert.assertTrue(result);
+
+            result = (boolean)runner.execute("null != null", context, null, false, false);
+            Assert.assertFalse(result);
+
+            List<String> expressionList = Arrays.asList(
+                "null > 2",
+                "null >= 2",
+                "null < 2",
+                "null <= 2",
+                "2 > null",
+                "2 >= null",
+                "2 < null",
+                "2 <= null"
+            );
+
+            for (String expression : expressionList) {
+                try {
+                    runner.execute(expression, context, null, false, false);
+                    Assert.fail(expression);
+                } catch (QLException e) {
+                }
+            }
+
+            QLExpressRunStrategy.setCompareNullLessMoreAsFalse(true);
+            for (String expression : expressionList) {
+                result = (boolean)runner.execute(expression, context, null, false, false);
+                Assert.assertFalse(result);
+            }
+        } finally {
+            QLExpressRunStrategy.setCompareNullLessMoreAsFalse(compareNullLessMoreAsFalse);
+        }
     }
 
     @Test
