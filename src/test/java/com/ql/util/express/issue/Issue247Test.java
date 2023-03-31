890463779d646d418008d5091ed03d339425da18
yumin.pym <yumin.pym@alibaba-inc.com>
2023-03-31 17:39:14 +0800

    add com.ql.util.express.issue.Issue247Test

---
+++ src/test/java/com/ql/util/express/issue/Issue247Test.java
@@ -0,0 +1,38 @@
+package com.ql.util.express.issue;
+
+import com.ql.util.express.DefaultContext;
+import com.ql.util.express.ExpressRunner;
+import com.ql.util.express.IExpressContext;
+import com.ql.util.express.config.QLExpressRunStrategy;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author 冰够
+ */
+public class Issue247Test {
+    private static boolean compareNullLessMoreAsFalse;
+
+    @BeforeClass
+    public static void beforeClass() {
+        compareNullLessMoreAsFalse = QLExpressRunStrategy.isCompareNullLessMoreAsFalse();
+        QLExpressRunStrategy.setCompareNullLessMoreAsFalse(true);
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        QLExpressRunStrategy.setCompareNullLessMoreAsFalse(compareNullLessMoreAsFalse);
+    }
+
+    @Test
+    public void test() throws Exception {
+        ExpressRunner runner = new ExpressRunner(false, true);
+        String expression = "2 > null";
+
+        IExpressContext<String, Object> context = new DefaultContext<>();
+        Object result = runner.execute(expression, context, null, false, false);
+        Assert.assertFalse((Boolean)result);
+    }
+}
