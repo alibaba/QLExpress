677728e25907444fbbce30cecc4b1250da173691
taokankan.tk <taokan1991@aliyun.com>
2023-04-01 17:18:38 +0800

    about safe function

--- src/main/java/com/alibaba/qlexpress4/Express4Runner.java
+++ src/main/java/com/alibaba/qlexpress4/Express4Runner.java
@@ -1,5 +1,20 @@
 package com.alibaba.qlexpress4;
 
+import com.alibaba.qlexpress4.cache.QLCaches;
+import com.alibaba.qlexpress4.exception.QLException;
+import com.alibaba.qlexpress4.parser.*;
+import com.alibaba.qlexpress4.parser.tree.Program;
+import com.alibaba.qlexpress4.runtime.*;
+import com.alibaba.qlexpress4.runtime.data.DataValue;
+import com.alibaba.qlexpress4.runtime.data.lambda.QLambdaMethod;
+import com.alibaba.qlexpress4.runtime.operator.CustomBinaryOperator;
+import com.alibaba.qlexpress4.runtime.operator.OperatorManager;
+import com.alibaba.qlexpress4.utils.CacheUtil;
+import com.alibaba.qlexpress4.utils.PropertiesUtil;
+import com.alibaba.qlexpress4.utils.QLFieldUtil;
+import com.alibaba.qlexpress4.utils.QLFunctionUtil;
+import org.apache.commons.lang.exception.ExceptionUtils;
+
 import java.lang.reflect.Array;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
@@ -10,21 +25,6 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
-
-import com.alibaba.qlexpress4.cache.*;
-import com.alibaba.qlexpress4.exception.QLException;
-import com.alibaba.qlexpress4.parser.*;
-import com.alibaba.qlexpress4.parser.tree.Program;
-import com.alibaba.qlexpress4.runtime.*;
-import com.alibaba.qlexpress4.runtime.data.DataValue;
-import com.alibaba.qlexpress4.runtime.data.lambda.QLambdaMethod;
-import com.alibaba.qlexpress4.runtime.instruction.QLInstruction;
-import com.alibaba.qlexpress4.runtime.operator.CustomBinaryOperator;
-import com.alibaba.qlexpress4.runtime.operator.OperatorManager;
-import com.alibaba.qlexpress4.utils.CacheUtil;
-import com.alibaba.qlexpress4.utils.PropertiesUtil;
-import com.alibaba.qlexpress4.utils.QLFieldUtil;
-import com.alibaba.qlexpress4.utils.QLFunctionUtil;
 
 /**
  * Author: DQinYuan
@@ -39,10 +39,10 @@ public class Express4Runner {
 
     public Express4Runner(InitOptions initOptions) {
         qlCaches = new QLCaches(CacheUtil.initConstructorCache(10, initOptions.enableUseCacheClear()),
-            CacheUtil.initFieldCache(10, initOptions.enableUseCacheClear()),
-            CacheUtil.initMethodCache(10, initOptions.enableUseCacheClear()),
-            CacheUtil.initMethodInvokeCache(10, initOptions.enableUseCacheClear()),
-            CacheUtil.initScriptCache(10, initOptions.enableUseCacheClear()));
+                CacheUtil.initFieldCache(10, initOptions.enableUseCacheClear()),
+                CacheUtil.initMethodCache(10, initOptions.enableUseCacheClear()),
+                CacheUtil.initMethodInvokeCache(10, initOptions.enableUseCacheClear()),
+                CacheUtil.initScriptCache(10, initOptions.enableUseCacheClear()));
     }
 
     public Object execute(String script, Map<String, Object> context, QLOptions qlOptions) throws QLException {
@@ -63,7 +63,7 @@ public class Express4Runner {
 
     public <T, R> void addFunction(String name, Function<T, R> function) {
         addFunction(name, new QFunctionInner(params -> {
-            R result = function.apply((T)params[0]);
+            R result = function.apply((T) params[0]);
             return new QResult(new DataValue(result), QResult.ResultType.RETURN);
         }));
     }
@@ -74,14 +74,14 @@ public class Express4Runner {
             for (int i = 0; i < params.length; i++) {
                 Array.set(array, i, params[i]);
             }
-            R result = functionalVarargs.call((T[])array);
+            R result = functionalVarargs.call((T[]) array);
             return new QResult(new DataValue(result), QResult.ResultType.RETURN);
         }));
     }
 
     public <T> void addFunction(String name, Predicate<T> predicate) {
         addFunction(name, new QFunctionInner(
-            params -> new QResult(new DataValue(predicate.test((T)params[0])), QResult.ResultType.RETURN)));
+                params -> new QResult(new DataValue(predicate.test((T) params[0])), QResult.ResultType.RETURN)));
     }
 
     public void addFunction(String name, Runnable runnable) {
@@ -93,7 +93,7 @@ public class Express4Runner {
 
     public <T> void addFunction(String name, Consumer<T> consumer) {
         addFunction(name, new QFunctionInner(params -> {
-            consumer.accept((T)params[0]);
+            consumer.accept((T) params[0]);
             return new QResult(null, QResult.ResultType.RETURN);
         }));
     }
@@ -120,20 +120,20 @@ public class Express4Runner {
 
     public void addFunction(String name, Object obj, String methodName) {
         if (obj instanceof Class) {
-            addFunctionByClass(name, (Class<?>)obj, methodName);
+            addFunctionByClass(name, (Class<?>) obj, methodName);
         } else {
             addFunctionByObject(name, obj, methodName);
         }
     }
 
     private void addFunctionByObject(String name, Object object, String methodName) {
-        List<Method> methods = PropertiesUtil.getMethod(object.getClass(), methodName, false);
-        addFunction(name, new QFunctionInner(new QLambdaMethod(methods, object, false)));
+        List<Method> methods = PropertiesUtil.getMethod(object.getClass(), methodName, true);
+        addFunction(name, new QFunctionInner(new QLambdaMethod(methods, object, true)));
     }
 
     private void addFunctionByClass(String name, Class<?> clazz, String methodName) {
-        List<Method> methods = PropertiesUtil.getClzMethod(clazz, methodName, false);
-        addFunction(name, new QFunctionInner(new QLambdaMethod(methods, clazz, false)));
+        List<Method> methods = PropertiesUtil.getClzMethod(clazz, methodName, true);
+        addFunction(name, new QFunctionInner(new QLambdaMethod(methods, clazz, true)));
     }
 
     private void addFunctionByAnnotation(Class<?> clazz, Object object) {
@@ -143,7 +143,7 @@ public class Express4Runner {
                 for (String value : QLFunctionUtil.getQLFunctionValue(method)) {
                     List<Method> qlMethods = new ArrayList<>();
                     qlMethods.add(method);
-                    addFunction(value, new QFunctionInner(new QLambdaMethod(qlMethods, object, false)));
+                    addFunction(value, new QFunctionInner(new QLambdaMethod(qlMethods, object, true)));
                 }
             }
         }
@@ -156,7 +156,7 @@ public class Express4Runner {
                 for (String value : QLFieldUtil.getQLFieldValue(method)) {
                     List<Method> qlMethods = new ArrayList<>();
                     qlMethods.add(method);
-                    addField(value, new QFunctionInner(new QLambdaMethod(qlMethods, object, false)));
+                    addField(value, new QFunctionInner(new QLambdaMethod(qlMethods, object, true)));
                 }
             }
         }
@@ -164,12 +164,29 @@ public class Express4Runner {
 
     public Program parseToSyntaxTree(String script, QLOptions qlOptions) {
         QLParser qlParser = new QLParser(operatorManager.getOperatorPrecedenceMap(), new Scanner(script, qlOptions),
-            ImportManager.buildGlobalImportManager(qlOptions.getDefaultImport()),
-            DefaultClassSupplier.INSTANCE);
+                ImportManager.buildGlobalImportManager(qlOptions.getDefaultImport()),
+                DefaultClassSupplier.INSTANCE);
         return qlParser.parse();
     }
 
+    public boolean addOperator(String operator, CustomBinaryOperator customBinaryOperator) {
+        return operatorManager.addOperator(operator, customBinaryOperator, QLPrecedences.MULTI);
+    }
+
+    public boolean addOperator(String operator, CustomBinaryOperator customBinaryOperator, int priority) {
+        return operatorManager.addOperator(operator, customBinaryOperator, priority);
+    }
+
     private QLambda parseToLambda(String script, Map<String, Object> context, QLOptions qlOptions) {
+        QLambdaDefinitionInner mainLambdaDefine = parseSyntaxAndInstructions(script, qlOptions);
+        QvmRuntime qvmRuntime = new QvmRuntime(qlOptions.getAttachments(), qlCaches, System.currentTimeMillis());
+        QvmGlobalScope globalScope = new QvmGlobalScope(context, userDefineFunction,
+                qlOptions.isPolluteUserContext());
+        return mainLambdaDefine.toLambda(new DelegateQContext(qvmRuntime, globalScope),
+                qlOptions, true);
+    }
+
+    private QLambdaDefinitionInner parseSyntaxAndInstructions(String script, QLOptions qlOptions) {
         Program program = parseToSyntaxTree(script, qlOptions);
         if (qlOptions.isDebug()) {
             qlOptions.getDebugInfoConsumer().accept("\nAST:");
@@ -183,23 +200,23 @@ public class Express4Runner {
         QLambdaDefinitionInner mainLambdaDefine = new QLambdaDefinitionInner("main",
                 qvmInstructionGenerator.getInstructionList(), Collections.emptyList(),
                 qvmInstructionGenerator.getMaxStackSize());
-        if (qlOptions.isDebug()) {
-            qlOptions.getDebugInfoConsumer().accept("\nInstructions:");
-            mainLambdaDefine.println(0, qlOptions.getDebugInfoConsumer());
+        return mainLambdaDefine;
+    }
+
+    /**
+     * 预热接口
+     *
+     * @param script
+     * @param qlOptions
+     * @return
+     */
+    public boolean checkSyntax(String script, QLOptions qlOptions) {
+        try {
+            parseSyntaxAndInstructions(script, qlOptions);
+            return true;
+        } catch (Exception e) {
+            qlOptions.getDebugInfoConsumer().accept("checkSyntax has exception:" + ExceptionUtils.getFullStackTrace(e));
+            return false;
         }
-
-        QvmRuntime qvmRuntime = new QvmRuntime(qlOptions.getAttachments(), qlCaches, System.currentTimeMillis());
-        QvmGlobalScope globalScope = new QvmGlobalScope(context, userDefineFunction,
-                qlOptions.isPolluteUserContext());
-        return mainLambdaDefine.toLambda(new DelegateQContext(qvmRuntime, globalScope),
-                qlOptions, true);
-    }
-
-    public boolean addOperator(String operator, CustomBinaryOperator customBinaryOperator) {
-        return operatorManager.addOperator(operator, customBinaryOperator, QLPrecedences.MULTI);
-    }
-
-    public boolean addOperator(String operator, CustomBinaryOperator customBinaryOperator, int priority) {
-        return operatorManager.addOperator(operator, customBinaryOperator, priority);
     }
 }
