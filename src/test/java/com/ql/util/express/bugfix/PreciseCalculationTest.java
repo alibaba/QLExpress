72b7d4e2d8aa92987f6a4dad004e02f43269c500
xiaolei55 <zl_1171807002@163.com>
2023-05-25 12:32:31 +0800

    修复issues-264的问题及优化计算溢出

---
+++ src/test/java/com/ql/util/express/bugfix/PreciseCalculationTest.java
@@ -0,0 +1,38 @@
+package com.ql.util.express.bugfix;
+
+import com.ql.util.express.DefaultContext;
+import com.ql.util.express.ExpressRunner;
+import org.apache.commons.math3.fraction.BigFraction;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+
+/**
+ * @author zhanglei
+ * @date 2023-05-25.
+ */
+public class PreciseCalculationTest {
+
+    @Test
+    public void testPrecise() throws Exception {
+        String exp = "a/b*c";
+		ExpressRunner runner = new ExpressRunner(true, false);
+		DefaultContext<String, Object> context = new DefaultContext<String, Object>();
+		context.put("a", new BigFraction(20));
+		context.put("b", new BigFraction(6));
+		context.put("c", new BigFraction(3));
+		Object result = runner.execute(exp, context, null, false, false);
+		System.out.println(result);
+    }
+
+    @Test
+    public void testResultOverflow() throws Exception {
+        String evalExpress = "a*2";
+        final ExpressRunner runner = new ExpressRunner(true, false);
+        DefaultContext<String, Object> nameMap = new DefaultContext<String, Object>();
+        nameMap.put("a", new BigDecimal(Long.MAX_VALUE));
+        Object obj = runner.execute(evalExpress, nameMap, null, false, false);
+        System.out.println(obj);
+    }
+}
+
