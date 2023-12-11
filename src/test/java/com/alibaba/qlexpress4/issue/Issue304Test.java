b9a2c18077ee50da327783cf165d431361c8dc03
冰够 <yumin.pym@alibaba-inc.com>
2023-12-11 20:38:09 +0800

    modify com.alibaba.qlexpress4.issue.Issue304Test

--- src/test/java/com/alibaba/qlexpress4/issue/Issue304Test.java
+++ src/test/java/com/alibaba/qlexpress4/issue/Issue304Test.java
@@ -18,8 +18,9 @@ public class Issue304Test {
     @Test
     public void test() throws Exception {
         String script
-            = "(((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((1+1))))))))))))"
-            + ")))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))";
+            = "(((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((("
+            + "1+1"
+            + "))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))";
         Express4Runner express4Runner = new Express4Runner(InitOptions.DEFAULT_OPTIONS);
         QLambda qLambda = express4Runner.parseToLambda(script, new MapExpressContext(new HashMap<>()),
             QLOptions.DEFAULT_OPTIONS);
