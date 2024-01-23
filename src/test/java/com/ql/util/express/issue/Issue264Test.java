b37618413501337c721a65021f3bea3b6bb4db6a
冰够 <yumin.pym@alibaba-inc.com>
2024-01-23 18:40:56 +0800

    com.ql.util.express.issue.Issue264Test

--- src/test/java/com/ql/util/express/issue/Issue264Test.java
+++ src/test/java/com/ql/util/express/issue/Issue264Test.java
@@ -10,20 +10,38 @@ import org.junit.Test;
  * @author 冰够
  */
 public class Issue264Test {
+    /**
+     * isPrecise:false, result:10.0              , result == 10.0:true
+     * isPrecise:false, result:9.9999999999      , d1 == d2:false
+     *
+     * isPrecise:true, result:9.9999999999      , result == 10.0:false
+     * isPrecise:true, result:9.9999999999      , d1 == d2:true
+     *
+     * @throws Exception
+     */
     @Test
     public void test() throws Exception {
         String exp = "a/b*c";
-        ExpressRunner runner = new ExpressRunner(true, false);
+        boolean isPrecise = true;
+        ExpressRunner runner = new ExpressRunner(isPrecise, false);
         DefaultContext<String, Object> context = new DefaultContext<>();
-        context.put("a", 20);
+        context.put("a", 20.0);
         context.put("b", 6);
         context.put("c", 3);
         Object result = runner.execute(exp, context, null, false, false);
-        System.out.println(result);
+        Double d1 = null;
+        if (result instanceof Double) {
+            d1 = (Double)result;
+        }
+        if (result instanceof BigDecimal) {
+            d1 = ((BigDecimal)result).doubleValue();
+        }
+        System.out.printf("isPrecise:%s, result:%-18s, result == 10.0:%s\n", isPrecise, d1, d1 == 10.0);
 
-        BigDecimal result2 = new BigDecimal("20.0")
+        BigDecimal bigDecimal = new BigDecimal("20.0")
             .divide(new BigDecimal("6"), 10, BigDecimal.ROUND_HALF_UP)
             .multiply(new BigDecimal("3"));
-        System.out.println("result2 = " + result2);
+        double d2 = bigDecimal.doubleValue();
+        System.out.printf("isPrecise:%s, result:%-18s, d1 == d2:%s\n", isPrecise, d2, d1 == d2);
     }
 }
