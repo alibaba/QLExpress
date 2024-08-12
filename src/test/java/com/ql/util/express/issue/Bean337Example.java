4bcc89f16d13b67e235a290a6b2b8a6ecafc191c
guojiaxiong399 <guojiaxiong399@hellobike.com>
2024-08-12 15:16:22 +0800

    fix comment in import

---
+++ src/test/java/com/ql/util/express/issue/Bean337Example.java
@@ -0,0 +1,7 @@
+package com.ql.util.express.issue;
+
+public class Bean337Example {
+    public static String getValue() {
+        return "successful";
+    }
+}
---
+++ src/test/java/com/ql/util/express/issue/Issue337CommentInImportTest.java
@@ -0,0 +1,27 @@
+package com.ql.util.express.issue;
+
+import com.ql.util.express.DefaultContext;
+import com.ql.util.express.ExpressRunner;
+import junit.framework.TestCase;
+import org.junit.Test;
+
+/**
+ * @author gjx
+ */
+public class Issue337CommentInImportTest {
+    @Test
+    public void testCommentInImport() throws Exception {
+        String express = ""
+                + "/** 注释 **/\n"
+                + "import java.util.ArrayList;"
+                + "import com.ql.util.express.issue.Bean337Example;"
+                + "abc = new Bean337Example();"
+                + "return abc.getValue();";
+        ExpressRunner runner = new ExpressRunner(false, true);
+        DefaultContext<String, Object> context = new DefaultContext<>();
+        Object result = runner.execute(express, context, null, false, true);
+        TestCase.assertEquals("successful", result);
+        System.out.println(result);
+        System.out.println(context);
+    }
+}
