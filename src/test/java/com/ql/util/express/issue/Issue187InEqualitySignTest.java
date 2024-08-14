90a87c288d2099037ef94fc2919c779d6d47e782
guojiaxiong399 <guojiaxiong399@hellobike.com>
2024-08-14 11:22:59 +0800

    support <>

---
+++ src/test/java/com/ql/util/express/issue/Issue187InEqualitySignTest.java
@@ -0,0 +1,22 @@
+package com.ql.util.express.issue;
+
+import com.ql.util.express.DefaultContext;
+import com.ql.util.express.ExpressRunner;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author gjx
+ */
+public class Issue187InEqualitySignTest {
+    @Test
+    public void test() throws Exception {
+        ExpressRunner runner = new ExpressRunner(false, true);
+        DefaultContext<String, Object> context = new DefaultContext<>();
+
+        String express = "1 <> 2";
+        Object result = runner.execute(express, context, null, true, true);
+        System.out.println(result);
+
+    }
+}
