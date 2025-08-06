80849455793cdb4874dfe3cdfd905cb749ba2061
lunasaw <iszychen@gmail.com>
2025-08-06 20:56:41 +0800

    fix: Support primitive to Object boxing conversion in method resolution

--- src/test/java/com/alibaba/qlexpress4/TestSuiteRunner.java
+++ src/test/java/com/alibaba/qlexpress4/TestSuiteRunner.java
@@ -61,7 +61,7 @@ public class TestSuiteRunner {
     @Test
     public void featureDebug()
         throws URISyntaxException, IOException {
-        Path filePath = getTestSuiteRoot().resolve("java/property/final_fix_solution.ql");
+        Path filePath = getTestSuiteRoot().resolve("java/property/null_set_invoke.ql");
         handleFile(filePath, filePath.toString(), true);
     }
 
